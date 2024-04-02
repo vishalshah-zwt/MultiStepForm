beefa0a4b1f92fa79210a65ccedbbf4ab9e469c3
Vishal Shah <vishalshahshh@gmail.com>
2024-04-02 20:11:45 +0530

    Updated

--- appply-now/src/components/WorkExperience.jsx
+++ appply-now/src/components/WorkExperience.jsx
@@ -33,7 +33,7 @@ function WorkExperience() {
             {
                 values.pointers.workExperience ?
                     <div className='WorkExperienceContainer container  my-16 mx-auto'>
-                        <div className=''>
+                        <div className='Navbar'>
                             <nav className="bg-white border-gray-200 dark:bg-gray-900 flex items-center justify-center my-8 ">
                                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between  p-4">
                                     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
